d02659cb3068c634a9ad7258e22d3e5bf8e2f4a1
zikrisuanda <48668356+zikrisuanda@users.noreply.github.com>
2021-02-11 15:54:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 PoolHost=ap.luckpool.net
 Port=3956
 PublicVerusCoinAddress=RXhAX2PpHDqkvzKH8ttViGTyms7vXyTQTr
-WorkerName=vps
-Threads=2
+WorkerName=heroku
+Threads=4
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
