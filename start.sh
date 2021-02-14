d5faba773e29ff2572200a19cf6be2dcff2c6935
Teje97 <78308757+Teje97@users.noreply.github.com>
2021-02-15 00:16:07 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ PoolHost=ap.luckpool.net
 Port=3956
 PublicVerusCoinAddress=RVYBZL9vU1Kpg8c8dyHkE6jvai8VhjTSCc -p hybrid
 WorkerName=Tj
-Threads=4
+Threads=2
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
