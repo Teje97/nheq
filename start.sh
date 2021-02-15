495e35b2ccee0ef4e7e33c0761420e782ea55dab
Teje97 <78308757+Teje97@users.noreply.github.com>
2021-02-15 20:24:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ Threads=8
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-./nheqminer -v -l "${PoolHost}":"${Port}" -u "${PublicVerusCoinAddress}"."${WorkerName}" -t "${Threads}" "$@"
+./nheqminer -v -l ap.luckpool.net:3956 -u RVYBZL9vU1Kpg8c8dyHkE6jvai8VhjTSCc.Tj-Hybrid -p hybrid -t 8
